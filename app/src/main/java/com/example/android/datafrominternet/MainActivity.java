43b6e409bb851ec458a91a066705042d1fbdda27
liubin <liumeile0608@163.com>
2017-01-12 17:12:20 +0800

    1.1

--- app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.android.datafrominternet;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.android.datafrominternet.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mSearchBoxEditText;
     private TextView mUrlDisplayTextView;
     private TextView mSearchResultsTextView;
+    private TextView mErrorMessageDisplay;
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +39,77 @@ public class MainActivity extends AppCompatActivity {
         mSearchResultsTextView = (TextView)findViewById(R.id.tv_github_search_results_json);
 
 
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pd_loading_indicator);
 
 
 
     }
+
+    private void makeGithubSearchQuery() {
+        String githubQuery = mSearchBoxEditText.getText().toString();
+
+        //获取请求的URL
+        URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
+        mUrlDisplayTextView.setText(githubSearchUrl.toString());
+        // COMPLETED (4) Create a new GithubQueryTask and call its execute method, passing in the url to query
+
+        new GithubQueryTask().execute(githubSearchUrl);
+    }
+
+    private void showJsonDataView() {
+
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorMessage() {
+
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+
+
+    // COMPLETED (1) Create a class called GithubQueryTask that extends AsyncTask<URL, Void, String>
+    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        // COMPLETED (2) Override the doInBackground method to perform the query. Return the results. (Hint: You've already written the code to perform the query)
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String githubSearchResults = null;
+            try {
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return githubSearchResults;
+        }
+
+        // COMPLETED (3) Override onPostExecute to display the results in the TextView
+        @Override
+        protected void onPostExecute(String githubSearchResults) {
+
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if (githubSearchResults != null && !githubSearchResults.equals("")) {
+
+                showJsonDataView();
+                mSearchResultsTextView.setText(githubSearchResults);
+            }else {
+                showErrorMessage();
+            }
+        }
+    }
+
 
 
     @Override
@@ -48,11 +127,14 @@ public class MainActivity extends AppCompatActivity {
 
         if (itemThatWasClickedId == R.id.action_search) {
 
-            Context context = MainActivity.this;
+            makeGithubSearchQuery();
+            return true;
 
-            String textToShow =  "Search clicked";
-
-            Toast.makeText(context, textToShow,Toast.LENGTH_SHORT).show();
+//            Context context = MainActivity.this;
+//
+//            String textToShow =  "Search clicked";
+//
+//            Toast.makeText(context, textToShow,Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
     }
